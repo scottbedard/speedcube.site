c925f6053d0a61bf552a9a6957961d62de2f04be
Scott Bedard <scottbedard90@gmail.com>
2018-11-04 19:19:24 -0700

    add signout link

--- themes/site/src/layouts/default/header/desktop_nav/user/user.vue
+++ themes/site/src/layouts/default/header/desktop_nav/user/user.vue
@@ -20,10 +20,10 @@
             class="flex items-center"
             href="#"
             @click.prevent="expand">
-            <i class="fa fa-angle-down mr-2 text-grey trans-color"></i>
             <div class="inline-block bg-white flex h-10 items-center justify-center rounded-full text-grey-dark trans-color w-10">
                 <i class="fa fa-user-o text-xl"></i>
             </div>
+            <i class="fa fa-angle-down ml-2 text-grey trans-color"></i>
         </a>
 
         <v-collapse-transition>
@@ -31,12 +31,24 @@
                 v-if="isExpanded"
                 class="absolute pin-r pin-t-full w-70">
                 <v-card>
-                    <!-- sign out -->
-                    <router-link
-                        class="block p-6"
-                        :to="{ name: 'signout' }">
-                        Sign out
-                    </router-link>
+                    <!-- profile -->
+                    <div class="p-6">
+                        <div
+                            v-text="user.name || 'Anonymous'"
+                            class="font-bold mb-px"
+                        />
+                        <div
+                            v-text="user.email"
+                            class="text-grey-dark text-sm"
+                        />
+                    </div>
+
+                    <div class="text-sm px-6 pb-6">
+                        <!-- sign out -->
+                        <v-user-link :to="{ name: 'signout' }">
+                            Sign out
+                        </v-user-link>
+                    </div>
                 </v-card>
             </div>
         </v-collapse-transition>
@@ -46,6 +58,7 @@
 <script>
 import { bindExternalEvent } from '@/app/utils/component';
 import { mapState } from 'vuex';
+import userLinkComponent from './user_link/user_link.vue';
 
 export default {
     data() {
@@ -55,6 +68,9 @@ export default {
     },
     mounted() {
         bindExternalEvent(this, document.body, 'click', this.onBodyClick);
+    },
+    components: {
+        'v-user-link': userLinkComponent,
     },
     computed: {
         ...mapState('user', [
---
+++ themes/site/src/layouts/default/header/desktop_nav/user/user_link/user_link.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    render(h, context) {
+        const { to } = context.props;
+        
+        return <router-link
+            to={to}>
+            {context.slots().default}
+        </router-link>;
+    },
+    functional: true,
+    props: [
+        'to',
+    ],
+};
+</script>
