1681460b924de3625ead2c0ed22771b46076279f
Scott Bedard <scottbedard90@gmail.com>
2018-12-27 21:44:23 -0700

    remove xhr test

--- themes/site/src/pages/home/home.vue
+++ themes/site/src/pages/home/home.vue
@@ -1,10 +1,6 @@
 <template>
     <v-margin class="text-center py-20" padded>
         <h1 class="font-light">This site is a work in progress, check back later.</h1>
-
-        <v-button @click="foo">
-            Trigger xhr
-        </v-button>
     </v-margin>
 </template>
 
@@ -13,9 +9,7 @@ import axios from 'axios';
 
 export default {
     methods: {
-        foo() {
-            axios.get('/foo');
-        }
-    }
-}
+        // ...
+    },
+};
 </script>
