5cd3e51a47ed2f866ed91052ab6169c3ff3cc0c5
Scott Bedard <scottbedard90@gmail.com>
2018-12-29 17:42:27 -0700

    nothing to see here

--- themes/site/src/components/ui/select.vue
+++ themes/site/src/components/ui/select.vue
@@ -1,13 +1,20 @@
 <script>
 export default {
     render(h, context) {
+        const { options } = context.props;
+
         return <div>
-            Hello
+            <select>
+
+            </select>
         </div>;
     },
     functional: true,
     props: {
-
+        options: {
+            default: () => [],
+            type: Array,
+        },
     },
 };
 </script>
