7a2cee00d72a8a2db7af301e590ad1132aa221b4
Scott Bedard <scottbedard90@gmail.com>
2019-01-16 18:17:26 -0700

    add escape key to abort a solve

--- themes/site/src/pages/solve/solve.vue
+++ themes/site/src/pages/solve/solve.vue
@@ -29,15 +29,17 @@
                             :started-at="inspectionStartedAt"
                             @complete="beginSolve"
                         />
+                        <p class="font-thin mt-4 text-grey-5">press space to start</p>
                     </div>
 
-                    <!-- solving -->
+                    <!-- solving / solved -->
                     <div v-else-if="solving || solved" key="solving">
                         <v-timer
                             :running="solving"
                             :display-time="solveCompletedTime"
                             :started-at="solveStartedAt"
                         />
+                        <p class="font-thin mt-4 mb-8 text-grey-5">press space to scramble</p>
                     </div>
 
                     <!-- idle -->
@@ -55,10 +57,14 @@
 </template>
 
 <script>
+import { bindExternalEvent } from '@/app/utils/component';
 import { postCreateScramble } from '@/app/repositories/scrambles';
 import { postSolve } from '@/app/repositories/solves';
 
 export default {
+    created() {
+        bindExternalEvent(this, document.body, 'keyup', this.onKeyup);
+    },
     data() {
         return {
             // log of all turns and solve events
@@ -78,6 +84,9 @@ export default {
 
             // scrambling phase
             scrambling: false,
+
+            // session of recent solves
+            solves: [],
 
             // solve completion time
             solveCompletedAt: 0,
@@ -105,8 +114,26 @@ export default {
         puzzleConfig() {
             return {};
         },
+        solution() {
+            return this.history.join(' ');
+        },
     },
     methods: {
+        abortSolve() {
+            this.history.push(`${Date.now()}#END`);
+            this.solving = false;
+
+            postSolve({
+                abort: true,
+                config: this.puzzleConfig,
+                scrambleId: this.scrambleId,
+                solution: this.solution,
+            }).then((response) => {
+                const { solve } = response.data;
+
+                this.solves.push(solve);
+            });
+        },
         beginInspection() {
             // allow partial turns
             this.scrambling = false;
@@ -140,8 +167,41 @@ export default {
             postSolve({
                 config: this.puzzleConfig,
                 scrambleId: this.scrambleId,
-                solution: this.history.join(' '),
+                solution: this.solution,
+            }).then((response) => {
+                // success
+                const { solve } = response.data;
+
+                this.solves.push(solve);
+            }, (err) => {
+                // failed
             });
+        },
+        onEscapeUp() {
+            // abort the current solve if one is running
+            if (this.inspecting || this.solving) {
+                this.abortSolve();
+            }
+        },
+        onKeyup(e) {
+            if (e.key === ' ') {
+                this.onSpaceUp();
+            } else if (e.key === 'Escape') {
+                this.onEscapeUp();
+            }
+        },
+        onSpaceUp() {
+            // start a new solve if we're not doing anything
+            if (!this.inspecting && !this.solving) {
+                this.scramble();
+            }
+
+            // if we're inspecting, transition to solving
+            // doing this removes our countdown from the dom, and this
+            // cancels the timeout from triggering a redundant start
+            if (this.inspecting) {
+                this.beginSolve();
+            }
         },
         recordTurn(turn) {
             if (this.inspecting || this.solving) {
