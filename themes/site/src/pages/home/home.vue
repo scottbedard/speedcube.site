31d5988de10dfe5e70ec77014dce4350394152d3
Scott Bedard <scottbedard90@gmail.com>
2019-02-23 12:30:49 -0700

    add a couple home page sections

--- themes/site/src/pages/home/home.vue
+++ themes/site/src/pages/home/home.vue
@@ -1,25 +1,45 @@
 <template>
     <v-page padded>
-        <v-margin class="text-center" padded>
-            <h1 class="font-thin mb-4 text-grey-8">
-               Welcome to the speedcube site
-            </h1>
-            <p class="font-thin leading-normal max-w-md mb-8 mx-auto text-grey-7 text-lg">
-                A free and open source place for speed cubing. We are currently in an open beta,
-                please <a href="https://github.com/scottbedard/speedcube.site/issues">report any issues</a>
-                and know that stats may be reset at any time.
-            </p>
-            <v-highlighted />
+        <v-margin padded>
+            <div class="mb-12 text-center md:mb-20">
+                <!-- heading -->
+                <h1 class="font-thin mb-8 text-grey-8 text-4xl">
+                    Welcome to the speedcube site
+                </h1>
+
+                <p class="font-thin leading-normal max-w-md mb-8 mx-auto text-grey-7 text-lg">
+                    A free and open source place for speed cubing. We are currently in an open beta,
+                    please <a href="https://github.com/scottbedard/speedcube.site/issues">report any issues</a>
+                    and know that stats may be reset at any time.
+                </p>
+
+                <!-- highlighted solve -->
+                <v-highlighted />
+            </div>
+
+            <!-- about -->
+            <div class="mb-12 md:mb-20">
+                <v-about />
+            </div>
+
+            <!-- stats -->
+            <div>
+                <v-stats />
+            </div>
         </v-margin>
     </v-page>
 </template>
 
 <script>
+import aboutComponent from './about/about.vue';
+import statsComponent from './stats/stats.vue';
 import highlightedComponent from './highlighted/highlighted.vue';
 
 export default {
     components: {
+        'v-about': aboutComponent,
         'v-highlighted': highlightedComponent,
+        'v-stats': statsComponent,
     },
 };
 </script>
---
+++ themes/site/src/pages/home/stats/stats.vue
@@ -0,0 +1,20 @@
+<template functional>
+    <v-grid padded>
+        <v-grid-cell class="md:order-2" md="7">
+            <h2 class="font-thin leading-normal mb-8 text-3xl text-grey-8 md:text-right">
+                Enjoy a detailed solve history
+            </h2>
+            <p class="font-thin leading-loose text-grey-7 md:text-right">
+                Every solve performed on the site is saved and analyzed. This is useful for visualizing
+                improvement over time, analyzed identifying algorithms that might need some practice. Forget about
+                tracking records in a notebook.<br class="hidden 2xl:inline" />
+                You just worry about the solve, we'll take care of the rest.
+            </p>
+        </v-grid-cell>
+        <v-grid-cell class="md:order-1" md="5">
+            <div class="text-center md:text-left">
+                <img class="h-auto max-w-sm w-full" src="./finance.svg" />
+            </div>
+        </v-grid-cell>
+    </v-grid>
+</template>
