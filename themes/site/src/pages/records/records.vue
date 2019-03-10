bc75789b30316aee169645c610473df7bdb4974d
Scott Bedard <scottbedard90@gmail.com>
2019-03-10 14:55:01 -0700

    begin scaffolding new records page

--- themes/site/src/pages/records/records.vue
+++ themes/site/src/pages/records/records.vue
@@ -1,35 +1,15 @@
 <template>
     <v-page padded>
-        <!-- title -->
-        <div class="mb-12 text-center">
-            <h1 class="font-thin mb-2 tracking-wide">
-                All Time Records
-            </h1>
-
-            <p class="font-thin text-grey text-xl">
-                These are the fastest solves ever performed.
-            </p>
-        </div>
-
-        <!-- nav -->
-        <div class="mb-12 text-center">
-            <v-nav />
-        </div>
-
-        <div class="max-w-xs mx-auto">
-            <v-event />
-        </div>
+        <v-margin padded>
+            Hello from the new records page!
+        </v-margin>
     </v-page>
 </template>
 
 <script>
-import navComponent from './nav/nav.vue';
-import eventComponent from './event/event.vue';
-
 export default {
     components: {
-        'v-nav': navComponent,
-        'v-event': eventComponent,
+        // ...
     },
 };
 </script>
