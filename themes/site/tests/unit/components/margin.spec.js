96e2f15313ede4fdb38e81498850c8547a7fd10e
Scott Bedard <scottbedard90@gmail.com>
2019-05-04 14:25:19 -0700

    migrating more tests to jest

--- themes/site/tests/unit/components/margin.spec.js
+++ themes/site/tests/unit/components/margin.spec.js
@@ -3,15 +3,15 @@
 //
 describe('<v-margin>', function() {
     it('renders default slot content', function() {
-        vm = mount({
+        const vm = mount({
             template: `<v-margin>Hello world</v-margin>`,
         });
 
-        expect(vm.$el.textContent).to.equal('Hello world');
+        expect(vm.$el.textContent).toBe('Hello world');
     });
 
     it('adds horizontal padding when padded prop is true', function(done) {
-        vm = mount({
+        const vm = mount({
             data() {
                 return {
                     padded: false,
@@ -23,12 +23,12 @@ describe('<v-margin>', function() {
         const hasPxClass = el => el.className.split(' ').filter(c => /^px-\d+$/g.test(c)).length > 0;
 
         // we should not have a px-# class
-        expect(hasPxClass(vm.$el)).to.be.false;
+        expect(hasPxClass(vm.$el)).toBe(false);
 
         // and flipping padded to true should attach the class
         vm.padded = true;
         vm.$nextTick(() => {
-            expect(hasPxClass(vm.$el)).to.be.true;
+            expect(hasPxClass(vm.$el)).toBe(true);
             done();
         });
     });
