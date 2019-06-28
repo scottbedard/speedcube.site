5fa92d97d7d19d9c80d6b09040429e6d2efd5210
Scott Bedard <scottbedard90@gmail.com>
2019-06-27 23:51:54 -0700

    add tests for browser logic

--- themes/site/src/components/renderer/renderer.vue
+++ themes/site/src/components/renderer/renderer.vue
@@ -1,21 +1,40 @@
 <template>
-    <div>
-        Renderer
-    </div>
+    <canvas
+        class="absolute h-full pin-l pin-t pointer-events-none w-full"
+        :height="height"
+        :style="{
+            transform: `translateY(${scrollY}px)`,
+        }"
+        :width="width"
+    />
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import { bindExternalEvent } from 'spyfu-vue-utils';
+
 export default {
     created() {
+        // listen for scroll events on the window
+        bindExternalEvent(this, window, 'scroll', () => {
+            this.scrollY = window.scrollY;
+        });
+
+        // listen for scene events
         this.$root.$on('scene-created', this.addScene);
         this.$root.$on('scene-destroyed', this.removeScene);
     },
     data() {
         return {
             scenes: [],
+            scrollY: window.scrollY,
         };
     },
     computed: {
+        ...mapState('browser', {
+            height: state => state.dimensions.height || 0,
+            width: state => state.dimensions.width || 0,
+        }),
         empty() {
             return this.scenes.length === 0;
         },
--- themes/site/tests/unit/renderer/renderer.spec.js
+++ themes/site/tests/unit/renderer/renderer.spec.js
@@ -16,7 +16,11 @@ const mount = factory({
 // specs
 //
 describe('<v-renderer>', () => {
-    it('registers and unregisters scenes', async () => {
+    beforeEach(() => {
+        global.window.scrollY = 0;
+    });
+
+    it('manages scenes', async () => {
         const vm = mount({
             data() {
                 return {
@@ -46,5 +50,46 @@ describe('<v-renderer>', () => {
 
         expect(vm.$refs.renderer.scenes.length).toBe(0);
         expect(vm.$refs.renderer.empty).toBe(true);
+    });
+
+    it('tracks scroll position', async () => {
+        global.window.scrollY = 10;
+
+        const vm = mount({
+            template: `<v-renderer ref="renderer" />`,
+        });
+
+        expect(vm.$refs.renderer.scrollY).toBe(10);
+        expect(global.window.getComputedStyle(vm.$el).getPropertyValue('transform')).toBe('translateY(10px)');
+
+        global.window.scrollY = 20;
+        global.window.dispatchEvent(new Event('scroll'));
+
+        await vm.$nextTick();
+
+        expect(vm.$refs.renderer.scrollY).toBe(20);
+        expect(global.window.getComputedStyle(vm.$el).getPropertyValue('transform')).toBe('translateY(20px)');
+    });
+
+    it('tracks browser dimensions', async () => {
+        const vm = mount({
+            template: `<v-renderer />`,
+        }, {
+            browser: {
+                dimensions: {
+                    height: 600,
+                    width: 800,
+                },
+            },
+        });
+
+        expect(Number(vm.$el.getAttribute('height'))).toBe(600);
+        expect(Number(vm.$el.getAttribute('width'))).toBe(800);
+
+        vm.$store.commit('browser/setDimensions', { height: 1000, width: 1200 });
+        await vm.$nextTick();
+
+        expect(Number(vm.$el.getAttribute('height'))).toBe(1000);
+        expect(Number(vm.$el.getAttribute('width'))).toBe(1200);
     });
 });
