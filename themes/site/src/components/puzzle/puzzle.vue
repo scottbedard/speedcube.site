79c419a4e6cae512e75975368d17ee8fc65b0c70
Scott Bedard <scottbedard90@gmail.com>
2019-06-30 22:16:41 -0700

    this breaks testing, we'll need to figure out another way

--- themes/site/src/components/puzzle/puzzle.vue
+++ themes/site/src/components/puzzle/puzzle.vue
@@ -1,3 +1,18 @@
 <template>
-    <div />
+    <div>
+        Puzzle
+        <v-scene>
+
+        </v-scene>
+    </div>
 </template>
+
+<script>
+import sceneComponent from '@/components/three/scene/scene.vue';
+
+export default {
+    components: {
+        'v-scene': sceneComponent,
+    },
+};
+</script>
--- themes/site/src/pages/components/examples/puzzle_example.vue
+++ themes/site/src/pages/components/examples/puzzle_example.vue
@@ -1,15 +1,19 @@
 <template>
     <v-example title="<v-puzzle>">
         <p>Our base component to manipulate and render puzzles.</p>
+
+        <v-puzzle />
     </v-example>
 </template>
 
 <script>
 import exampleComponent from '../example.vue';
+import puzzleComponent from '@/components/puzzle/puzzle.vue';
 
 export default {
     components: {
         'v-example': exampleComponent,
+        'v-puzzle': puzzleComponent,
     },
 };
 </script>
