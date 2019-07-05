9ac771a2df9cd7e2a686464ac5bf7ddb930dfb00
Scott Bedard <scottbedard90@gmail.com>
2019-07-05 15:06:06 -0700

    set camera position and respond to changes

--- themes/site/src/components/three/scene/scene.vue
+++ themes/site/src/components/three/scene/scene.vue
@@ -1,10 +1,14 @@
 <script>
-import { Scene } from 'three';
 import base from '../base';
+import { Scene, PerspectiveCamera } from 'three';
+import { degreesToRadians } from '@/app/utils/number';
 
 export default {
     created() {
         this.$options.three.obj = new Scene();
+        this.$options.three.camera = new PerspectiveCamera();
+
+        this.syncCameraPosition();
     },
     mounted() {
         this.$root.$emit('scene-add', this);
@@ -12,15 +16,40 @@ export default {
     destroyed() {
         this.$root.$emit('scene-remove', this);
     },
+    computed: {
+        cameraPosition() {
+            const angle = degreesToRadians(this.cameraAngle);
+            const adjacent = Math.sin(angle) * this.cameraDistance;
+            const opposite = Math.cos(angle) * this.cameraDistance;
+
+            return { opposite, adjacent };
+        },
+    },
     methods: {
-        
+        syncCameraPosition() {
+            const { camera } = this.$options.three;
+            const { opposite, adjacent } = this.cameraPosition;
+
+            camera.position.set(0, opposite, adjacent);
+            camera.lookAt(0, 0, 0);
+        },
     },
     mixins: [
         base,
     ],
     name: 'scene',
     props: {
-        // ...
+        cameraAngle: {
+            default: 0,
+            type: Number,
+        },
+        cameraDistance: {
+            default: 0,
+            type: Number,
+        },
+    },
+    watch: {
+        cameraPosition: 'syncCameraPosition',
     },
 };
 </script>
--- themes/site/tests/unit/three/scene.spec.js
+++ themes/site/tests/unit/three/scene.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import sceneComponent from '@/components/three/scene/scene.vue';
 import rendererComponent from '@/components/three/renderer/renderer.vue';
-import { Scene, WebGLRenderer } from 'three';
+import { WebGLRenderer } from 'three';
 
 //
 // factory
@@ -21,6 +21,8 @@ jest.mock('three', () => {
 
     return {
         ...three,
+
+        // WebGLRenderer
         WebGLRenderer: jest.fn(() => {
             return {
                 clear: jest.fn(),
@@ -40,10 +42,11 @@ jest.mock('three', () => {
 //
 describe('<v-scene>', () => {
     beforeEach(() => {
+        // PerspectiveCamera.mockClear();
         WebGLRenderer.mockClear();
     });
 
-    it('registers itself with the renderer', async () => {
+    it('adds and removes itself from the renderer', async () => {
         const vm = mount({
             data() {
                 return {
@@ -72,11 +75,67 @@ describe('<v-scene>', () => {
         expect(vm.$refs.renderer.$options.scenes).toEqual([]);
     });
 
-    it('instantiates a camera', () => {
+    it('instantiates a scene', () => {
         const vm = mount({
             template: `<v-scene ref="scene" />`,
         });
 
         expect(vm.$refs.scene.$options.three.obj.type).toBe('Scene');
+    });
+
+    it('instantiates a camera', () => {
+        const vm = mount({
+            template: `<v-scene ref="scene" />`,
+        });
+
+        expect(vm.$refs.scene.$options.three.camera.type).toBe('PerspectiveCamera');
+    });
+
+    it('sets the initial camera position and reacts to changes', async () => {
+        const vm = mount({
+            data() {
+                return {
+                    cameraAngle: 0,
+                    cameraDistance: 100,
+                };
+            },
+            template: `
+                <v-scene
+                    ref="scene"
+                    :camera-angle="cameraAngle"
+                    :camera-distance="cameraDistance"
+                />
+            `,
+        });
+
+        // our camera should exist at the correct following position
+        const { camera } = vm.$refs.scene.$options.three;
+        expect(camera.position).toEqual({ x: 0, y: 100, z: 0 });
+        
+        // change the camera angle
+        // we'll rotate 180deg to make the following assertion a bit easier
+        const lookAt = jest.spyOn(camera, 'lookAt');
+        const set = jest.spyOn(camera.position, 'set');
+
+        vm.cameraAngle = 180;
+        await vm.$nextTick();
+
+        // the camera should be repositioned
+        // @todo: improve this assertion with non-ridiculous values
+        expect(set).toHaveBeenCalledWith(0, expect.any(Number), expect.any(Number));
+        expect(lookAt).toHaveBeenCalledWith(0, 0, 0);
+        expect(camera.position.y).toBe(-100);
+
+        set.mockClear();
+        lookAt.mockClear();
+
+        // change the camera distance
+        // this will put us twice as far from the origin
+        vm.cameraDistance = 200;
+        await vm.$nextTick();
+
+        expect(set).toHaveBeenCalledWith(0, expect.any(Number), expect.any(Number));
+        expect(lookAt).toHaveBeenCalledWith(0, 0, 0);
+        expect(camera.position.y).toBe(-200);
     });
 });
