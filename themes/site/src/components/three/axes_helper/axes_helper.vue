124f06f0c364c152c2f18774c39f4ecf7172c022
Scott Bedard <scottbedard90@gmail.com>
2019-07-05 17:13:11 -0700

    convert axes helper to use threejs base

--- themes/site/src/components/three/axes_helper/axes_helper.vue
+++ themes/site/src/components/three/axes_helper/axes_helper.vue
@@ -1,29 +1,16 @@
 AxesHelper
 
 <script>
-import { noop } from 'lodash-es';
-import { findAncestor } from '@/app/utils/component';
 import { AxesHelper } from 'three';
+import base from '../base';
 
 export default {
     created() {
-        const scene = findAncestor(this, 'scene');
-        const axesHelper = new AxesHelper(this.size);
-
-        // add to scene
-        scene.$options.three.scene.add(axesHelper);
-
-        // remove from scene
-        this.$on('hook:destroyed', () => {
-            scene.$options.three.scene.remove(axesHelper);
-        });
-
-        // store our non-reactive state for later use
-        this.$options.three = {
-            axesHelper,
-        };
+        this.$options.three.obj = new AxesHelper(this.size);
     },
-    render: noop,
+    mixins: [
+        base,
+    ],
     props: {
         size: {
             default: 100,
