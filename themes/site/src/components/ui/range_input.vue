e23816bd895366c4eb9b119bd977edce773ed1e6
Scott Bedard <scottbedard90@gmail.com>
2019-07-14 15:48:19 -0700

    restore styling for range inputs

--- themes/site/src/components/ui/range_input.vue
+++ themes/site/src/components/ui/range_input.vue
@@ -1,3 +1,28 @@
+<style lang="scss" scoped>
+    // https://github.com/darlanrod/input-range-scss
+    $thumb-color: #186FAF;
+    // $thumb-border-color: #eceff1;
+    $thumb-border-width: 0px;
+    $thumb-height: 16px;
+    $thumb-radius: 8px;
+    $thumb-shadow-blur: 0px;
+    // $thumb-shadow-color: rgba(0, 0, 0, .2);
+    $thumb-shadow-size: 0px;
+
+    $thumb-width: 16px;
+    $track-border-width: 0px;
+    $track-color: #52606D;
+    // $track-border-color: #cfd8dc;
+    $track-height: 4px;
+    $track-radius: 2px;
+    $track-shadow-blur: 0px;
+    // $track-shadow-color: rgba(0, 0, 0, .2);
+    $track-shadow-size: 0px;
+    // $track-width: 100%;
+
+    @import '../../../node_modules/input-range-scss/_inputrange.scss';
+</style>
+
 <template>
     <input
         v-model.number="val"
