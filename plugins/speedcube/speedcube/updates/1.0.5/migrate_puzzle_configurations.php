2fe3c530e0188ea96d15c0141f15ea48deb2bdee
Scott Bedard <scottbedard90@gmail.com>
2019-07-26 22:39:56 -0700

    improving data migration

--- plugins/speedcube/speedcube/updates/1.0.5/migrate_puzzle_configurations.php
+++ plugins/speedcube/speedcube/updates/1.0.5/migrate_puzzle_configurations.php
@@ -13,6 +13,8 @@ class MigratePuzzleConfigurations extends Migration
 {
     public function up()
     {
+        // version >= 1.0.5 stores many config values as
+        // decimals, and default camera distance is shortened
         $this->migrateConfigs();
     }
 
@@ -22,34 +24,46 @@ class MigratePuzzleConfigurations extends Migration
 
     private function migrateConfigs()
     {
-        // $user = Factory::registerUser();
-
-        // $config = Config::create([
-        //     'config' => '{"colors":["#FFEE5D","#EFAA18","#2589E2","#EC6157","#5CBD60","#F0F0F0"],"stickerSpacing":18,"stickerElevation":100,"stickerRadius":9,"innerBrightness":82,"cameraAngle":45,"cameraDistance":2083,"turnDuration":82}',
-        //     'puzzle' => '3x3',
-        //     'user_id' => $user->id,
-        // ]);
-
-        // $solve = Factory::completedSolve($user);
-        // $solve->config = $config->config;
-        // $solve->save();
-
-        // version >= 1.0.5 will store config values as decimals
-        // when appropriate. these values are more useful when
-        // calculating sticker positioning and geometries. in
-        // addition to this, we've shortened max camera distance.
         $update = function ($config) {
+            $data = [];
             $original = json_decode($config, true);
 
-            return json_encode([
-                'cameraAngle' => $original['cameraAngle'],
-                'cameraDistance' => ($original['cameraDistance'] / 8000) * 3000,
-                'colors' => $original['colors'],
-                'stickerElevation' => $original['stickerElevation'] / 100,
-                'stickerRadius' => $original['stickerRadius'] / 100,
-                'stickerSpacing' => $original['stickerSpacing'] / 100,
-                'turnDuration' => $original['turnDuration'],
-            ]);
+            // camera angle
+            if (array_key_exists('cameraAngle', $original)) {
+                $data['cameraAngle'] = $original['cameraAngle'];
+            }
+
+            // camera distance
+            if (array_key_exists('cameraDistance', $original)) {
+                $data['cameraDistance'] = ($original['cameraDistance'] / 8000) * 1000;
+            }
+
+            // colors
+            if (array_key_exists('colors', $original)) {
+                $data['colors'] = $original['colors'];
+            }
+
+            // sticker elevation
+            if (array_key_exists('stickerElevation', $original)) {
+                $data['stickerElevation'] = $original['stickerElevation'] / 100;
+            }
+
+            // sticker radius
+            if (array_key_exists('stickerRadius', $original)) {
+                $data['stickerRadius'] = $original['stickerRadius'] / 100;
+            }
+
+            // sticker spacing
+            if (array_key_exists('stickerSpacing', $original)) {
+                $data['stickerSpacing'] = $original['stickerSpacing'] / 100;
+            }
+
+            // turn duration
+            if (array_key_exists('turnDuration', $original)) {
+                $data['turnDuration'] = $original['turnDuration'];
+            }
+
+            return json_encode($data);
         };
 
         // update user configs
