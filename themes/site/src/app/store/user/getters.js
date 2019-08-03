21ccdeb8c63d834a3d77843fe11525107528f86b
Scott Bedard <scottbedard90@gmail.com>
2019-08-03 10:18:36 -0700

    remove puzzle config from user getters

--- themes/site/src/app/store/user/getters.js
+++ themes/site/src/app/store/user/getters.js
@@ -4,39 +4,6 @@ import { isUndefined } from 'lodash-es';
 // getters
 //
 export default {
-    // default puzzle configuration
-    defaultConfig(state) {
-        const coreConfig = {
-            colors: [
-                '#ffeb3b', // U -> yellow
-                '#ff9800', // L -> orange
-                '#03a9f4', // F -> blue
-                '#f44336', // R -> red
-                '#4caf50', // B -> green
-                '#eeeeee', // D -> white
-            ],
-            stickerElevation: 0.03,
-            stickerInnerOpacity: 0.3,
-            stickerRadius: 0.1,
-            stickerScale: 0.9,
-            turnDuration: 100,
-        };
-
-        let defaultConfig = {};
-
-        // find the default config if the user has one
-        if (!isUndefined(state.user.configs)) {
-            const config = state.user.configs.find(cfg => cfg.puzzle === 'default');
-
-            if (config) {
-                defaultConfig = config.config;
-            }
-        }
-
-        // spread the user's default config over the core config
-        return { ...coreConfig, ...defaultConfig };
-    },
-
     // determine if the user has an avatar
     hasAvatar(state, getters) {
         return getters.isAuthenticated && state.user.avatar !== null;
@@ -45,24 +12,5 @@ export default {
     // determine if the user is authenticated
     isAuthenticated(state) {
         return !isUndefined(state.user.id);
-    },
-
-    // configuration for a specific puzzle
-    puzzleConfig(state, getters) {
-        const { defaultConfig } = getters;
-
-        return (puzzle) => {
-            let puzzleConfig = {};
-
-            if (!isUndefined(state.user.configs)) {
-                const config = state.user.configs.find(cfg => cfg.puzzle === puzzle);
-
-                if (config) {
-                    puzzleConfig = config.config;
-                }
-            }
-
-            return { ...defaultConfig, ...puzzleConfig };
-        };
     },
 };
