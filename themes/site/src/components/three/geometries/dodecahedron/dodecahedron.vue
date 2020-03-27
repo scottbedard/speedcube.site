0f9f2719b1a96479a0a083717e24241f3bd28685
Scott Bedard <scottbedard90@gmail.com>
2020-03-26 23:36:17 -0700

    scaffolding dodeahedron geometry

---
+++ themes/site/src/components/three/geometries/dodecahedron/dodecahedron.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="hidden">
+        <v-object :position="vertices[2]">
+            <slot name="u" />
+        </v-object>
+    </div>
+</template>
+
+<script>
+import { DodecahedronGeometry, Mesh, MeshBasicMaterial, Object3D } from 'three';
+import { computed } from '@vue/composition-api';
+import { threeProps, useDisposable, useThree } from '@/app/behaviors/three';
+import objectComponent from '../../object/object.vue';
+
+export default {
+    /**
+     * Setup.
+     *
+     * @return {void}
+     */
+    setup(props, context) {
+        const geometry = new DodecahedronGeometry(10);
+
+        const material = new MeshBasicMaterial({
+            color: 0xaaaaaa,
+            wireframe: true,
+        });
+        
+        const mesh = new Mesh(geometry, material);
+
+        const vertices = computed(() => geometry.vertices);
+
+        useDisposable(geometry, material);
+
+        const { getThreeObj } = useThree(mesh, {
+            context,
+            name: () => props.name,
+            position: () => props.position,
+            rotation: () => props.rotation,
+            scale: () => props.scale,
+        });
+
+        return {
+            getThreeObj,
+            vertices,
+        };
+    },
+    components: {
+        'v-object': objectComponent,
+    },
+    props: {
+        ...threeProps,
+        size: {
+            default: 10,
+            type: [Number, Object],
+        },
+    },
+};
+</script>
