3fc847d0fa75d606b74171d2f2b2b8de848689bb
Scott Bedard <scottbedard90@gmail.com>
2020-05-02 14:34:14 -0700

    scaffolding a group component

---
+++ themes/site/src/components/three/group/group.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script>
+import { Group } from 'three';
+import { useNesting } from 'vue-use-three';
+
+export default {
+    setup() {
+        const group = new Group();
+
+        useNesting(group);
+    },
+};
+</script>
