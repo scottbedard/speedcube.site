099f9bc41093eaa721965ac795b45526a764e7cd
Scott Bedard <scottbedard90@gmail.com>
2020-09-27 15:39:06 -0700

    cleanup

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,33 +6,14 @@
 
 <template>
   <div class="flex h-screen items-center justify-center p-6 text-center">
-    <div>
-      <h1 class="text-2xl">Soon...</h1>
-      <div>
-        Count: {{ count }}<br />
-        Doubled: {{ doubled }}<br />
-        <button @click="inc">+1</button>
-      </div>
-    </div>
+    <h1 class="text-2xl">Soon...</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { double } from '@/math';
-import { computed, defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  setup() {
-    const count = ref(0);
-    const inc = () => count.value++;
-    const doubled = computed(() => double(count.value));
-
-    return {
-      count,
-      doubled,
-      inc,
-    };
-  },
   name: 'App',
 });
 </script>
