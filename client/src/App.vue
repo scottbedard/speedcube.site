e35f4d1768885ba2e3a3147fd7869c4d183bad33
Scott Bedard <scottbedard90@gmail.com>
2020-09-26 23:44:02 -0700

    vue 3

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    <h1>Hello from the view application</h1>
+    <h1>Vue 3</h1>
+    <div>Count: {{ count }}</div>
+    <button @click="inc">+1</button>
   </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0);
+
+    const inc = () => count.value += 1;
+
+    return {
+      count,
+      inc,
+    };
+  },
+};
+</script>
