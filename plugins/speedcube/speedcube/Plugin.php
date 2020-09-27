3509d85bb17e2d9c91e5f52c6b9f37ca5de70375
Scott Bedard <scottbedard90@gmail.com>
2020-09-26 21:47:27 -0700

    begin scaffolding plugin

---
+++ plugins/speedcube/speedcube/Plugin.php
@@ -0,0 +1,61 @@
+<?php namespace Speedcube\Speedcube;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Speedcube Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'icon' => 'icon-cube',
+            'name' => 'Speedcube',
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'speedcube' => [
+                'icon' => 'icon-leaf',
+                'label' => 'Speedcube',
+                'order' => 500,
+                'permissions' => ['speedcube.speedcube.*'],
+                'url' => Backend::url('speedcube/speedcube/mycontroller'),
+            ],
+        ];
+    }
+}
