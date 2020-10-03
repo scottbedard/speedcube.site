bad3e98e11a9f010c0f91575b0d403a28489bc9d
Scott Bedard <scottbedard90@gmail.com>
2020-10-02 21:31:05 -0700

    create icon component

---
+++ client/src/components/icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg
+    v-bind="icon.attrs"
+    v-html="contents"
+    :stroke-width="stroke" />
+</template>
+
+<script>
+// https://feathericons.com
+import feather from 'feather-icons';
+
+export default {
+  computed: {
+    contents() {
+      return this.icon?.toString();
+    },
+    icon() {
+      return feather.icons[this.name];
+    },
+  },
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+    stroke: {
+      default: 2,
+      type: [Number, String],
+    },
+  },
+};
+</script>
