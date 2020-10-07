9e4902e9a4e987498af7562abdbbc5d0515de541
Scott Bedard <scottbedard90@gmail.com>
2020-10-06 22:02:50 -0700

    chunk names

--- client/src/pages/components/examples/index.ts
+++ client/src/pages/components/examples/index.ts
@@ -3,6 +3,10 @@
 // is Record<string, () => Promise<typeof import('*.vue')>>
 
 const examples: Record<string, () => Promise<any>> = {
+  '<v-button>': () => import('./button-example.vue' /* webpackChunkName: 'button-example' */),
+  '<v-card>': () => import('./card-example.vue' /* webpackChunkName: 'card-example' */),
+  '<v-input>': () => import('./input-example.vue' /* webpackChunkName: 'input-example' */),
+  '<v-range-input>': () => import('./range-input-example.vue' /* webpackChunkName: 'component-example' */),
 };
 
 export default examples;
