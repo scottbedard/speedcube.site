5766f396541d1e93be89ca7e83933acb887f5bb3
Scott Bedard <scottbedard90@gmail.com>
2020-10-10 20:51:43 -0700

    begin working on stickers

--- client/src/components/puzzle/cube.ts
+++ client/src/components/puzzle/cube.ts
@@ -3,12 +3,31 @@ import { isNumber } from 'lodash';
 import { useAxesHelper } from '@/app/three/utils/axes-helper';
 import { useBoxGeometry } from '@/app/three/geometries/box-geometry';
 import { useGroup } from '@/app/three/utils/group';
+import { usePosition } from '@/app/three/utils/position';
+import { DoubleSide, Mesh, MeshLambertMaterial, Shape, ShapeBufferGeometry } from 'three';
 
 // edge length of a cube inscribed in a sphere of radius 1
 const edgeLength = 2 / Math.sqrt(3);
 
+// create a rounded square shape buffer geometry
+function createStickerShape(size: number, radius: number) {
+  const shape = new Shape();
+
+  shape.moveTo(0, radius);
+  shape.lineTo(0, size - radius);
+  shape.quadraticCurveTo(0, size, radius, size);
+  shape.lineTo(size - radius, size);
+  shape.quadraticCurveTo(size, size, size, size - radius);
+  shape.lineTo(size, radius);
+  shape.quadraticCurveTo(size, 0, size - radius, 0);
+  shape.lineTo(radius, 0);
+  shape.quadraticCurveTo(0, 0, 0, radius);
+
+  return new ShapeBufferGeometry(shape);
+}
+
 // normalize cube options
-function normalize(opts: Record<string, any>) {
+function normalizeOptions(opts: Record<string, any>) {
   return {
     innerBrightness: isNumber(opts.innerBrightness) ? opts.innerBrightness : 0,
     stickerElevation: isNumber(opts.stickerElevation) ? opts.stickerElevation : 0,
@@ -22,9 +41,22 @@ function normalize(opts: Record<string, any>) {
  * Cube
  */
 export function useCube(model: Cube<Record<string, any>>, rawOpts: Record<string, any>) {
-  const opts = normalize(rawOpts);
+  const opts = normalizeOptions(rawOpts);
+  const layerSize = edgeLength / model.options.size;
 
-  console.log('cube', opts);
+  const sticker = createStickerShape(layerSize, opts.stickerRadius);
+
+  const material = new MeshLambertMaterial({
+    color: 0x00ff00,
+    side: DoubleSide,
+  });
+
+  const mesh = new Mesh(sticker, material);
+
+  usePosition(mesh, {
+    x: -(edgeLength / 2),
+    y: (edgeLength / 2) - layerSize,
+  });
 
   return useGroup([
     useBoxGeometry({
@@ -37,7 +69,9 @@ export function useCube(model: Cube<Record<string, any>>, rawOpts: Record<string
       slots: {
         top: useAxesHelper(),
         left: useAxesHelper(),
-        front: useAxesHelper(),
+        front: useGroup([
+          mesh,
+        ]),
         right: useAxesHelper(),
         back: useAxesHelper(),
         down: useAxesHelper(),
