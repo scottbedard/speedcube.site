ab833cb84636f00d093b8b9b29cb5b5e0d651d19
Scott Bedard <scottbedard90@gmail.com>
2020-10-17 15:13:59 -0700

    add sphere geometry

---
+++ client/src/components/three/geometries/sphere-geometry.vue
@@ -0,0 +1,49 @@
+<template>
+  <slot />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Color, Mesh, MeshLambertMaterial, SphereGeometry } from 'three';
+import { positionProp, usePosition } from '@/app/three/utils/position';
+import { useNesting } from '@/app/three/utils/nestable';
+
+export default defineComponent({
+  setup(props) {
+    const geometry = new SphereGeometry(props.radius, props.widthSegments, props.heightSegments);
+
+    const material = new MeshLambertMaterial({
+      color: new Color(props.color),
+      wireframe: props.wireframe,
+    });
+
+    const sphere = new Mesh(geometry, material);
+
+    useNesting(sphere);
+    usePosition(sphere, () => props.position);
+  },
+  props: {
+    color: {
+      default: '#ff0',
+      type: [Number, String],
+    },
+    heightSegments: {
+      default: 6,
+      type: Number,
+    },
+    position: positionProp,
+    radius: {
+      default: 1,
+      type: Number,
+    },
+    widthSegments: {
+      default: 8,
+      type: Number,
+    },
+    wireframe: {
+      default: false,
+      type: Boolean,
+    },
+  },
+});
+</script>
