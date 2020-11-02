3cc27b48d716c723b28daae189b326270d39494e
Scott Bedard <scottbedard90@gmail.com>
2020-11-01 20:27:41 -0700

    extract parent / child nesting behaviors to their own functions

--- client/src/app/three/behaviors/nesting.ts
+++ client/src/app/three/behaviors/nesting.ts
@@ -6,19 +6,34 @@ const ParentSymbol: InjectionKey<{
   remove(obj: Object3D): void,
 }> = Symbol('ParentSymbol');
 
+
 /**
  * Nesting context
  */
 export function useNesting(obj: Object3D, isRoot: boolean = false) {
   if (!isRoot) {
-    const parent = inject(ParentSymbol);
-
-    if (parent) {
-      onMounted(() => parent.add(obj));
-      onUnmounted(() => parent.remove(obj));
-    }
+    useChildContext(obj);
   }
 
+  useParentContext(obj);
+}
+
+/**
+ * Object is child of parent object
+ */
+export function useChildContext(obj: Object3D) {
+  const parent = inject(ParentSymbol);
+
+  if (parent) {
+    onMounted(() => parent.add(obj));
+    onUnmounted(() => parent.remove(obj));
+  }
+}
+
+/**
+ * Object is parent to child objects
+ */
+export function useParentContext(obj: Object3D) {
   provide(ParentSymbol, {
     add: (child) => obj.add(child),
     remove: (child) => obj.remove(child),
