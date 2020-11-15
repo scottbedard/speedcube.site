ed8b26af6d03c5e0522e215006c2d9f1965e9930
Scott Bedard <scottbedard90@gmail.com>
2020-11-14 22:03:19 -0700

    readability

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,41 +1,75 @@
 import { RouteRecordRaw } from 'vue-router';
 
+/**
+ * Application routes
+ */
 export const routes: RouteRecordRaw[] = [
+  //
+  // home
+  //
   {
     component: () => import('@/pages/home.vue' /* webpackChunkName: 'home' */),
     name: 'home',
     path: '/',
   },
+
+  //
+  // components
+  //
   {
     component: () => import('@/pages/components/components.vue' /* webpackChunkName: 'components' */),
     name: 'components',
     path: '/components',
   },
+
+  //
+  // login
+  //
   {
     component: () => import('@/pages/login.vue' /* webpackChunkName: 'login' */),
     name: 'login',
     path: '/login',
   },
+
+  //
+  // logout
+  //
   {
     component: () => import('@/pages/logout.vue' /* webpackChunkName: 'logout' */),
     name: 'logout',
     path: '/logout',
   },
+
+  //
+  // records
+  //
   {
     component: () => import('@/pages/records.vue' /* webpackChunkName: 'records' */),
     name: 'records',
     path: '/records',
   },
+
+  //
+  // sandbox
+  //
   {
     component: () => import('@/pages/sandbox.vue' /* webpackChunkName: 'sandbox' */),
     name: 'sandbox',
     path: '/sandbox',
   },
+
+  //
+  // signup
+  //
   {
     component: () => import('@/pages/signup.vue' /* webpackChunkName: 'signup' */),
     name: 'signup',
     path: '/signup',
   },
+
+  //
+  // solve
+  //
   {
     beforeEnter(to) {
       if (!to.params.id) {
