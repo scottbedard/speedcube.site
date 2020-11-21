e98e1880692c216b6f7f822f1ec98a0d9d1db512
Scott Bedard <scottbedard90@gmail.com>
2020-11-20 21:07:33 -0700

    begin scaffolding a color picker

---
+++ client/src/components/color-picker.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hello from the color picker
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'color-picker',
+});
+</script>
---
+++ client/src/pages/components/examples/color-picker-example.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-color-picker />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VColorPicker from '@/components/color-picker.vue';
+
+export default defineComponent({
+  components: {
+    VColorPicker,
+  },
+});
+</script>
