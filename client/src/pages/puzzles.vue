dd97a4312fa8a44c5fc23a8c841e23488419394a
Scott Bedard <scottbedard90@gmail.com>
2020-12-26 21:04:16 -0700

    basic links to puzzles

--- client/src/pages/puzzles.vue
+++ client/src/pages/puzzles.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>
-    Hello from the puzzles page
+  <h1 class="font-bold mb-6 text-center text-2xl">Select a puzzle</h1>
+  <div class="gap-6 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+    <router-link
+      v-for="[puzzle, model] in puzzles"
+      :key="puzzle.param"
+      :to="{
+        name: 'solve',
+        params: { puzzle },
+      }">
+      <v-puzzle :model="model" />
+    </router-link>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Cube, Dodecaminx } from '@bedard/twister';
+import VPuzzle from '@/components/puzzle/puzzle.vue';
+
+export default defineComponent({
+  setup() {
+    const puzzles = [
+      ['2x2', new Cube({ size: 2 })],
+      ['3x3', new Cube({ size: 3 })],
+      ['4x4', new Cube({ size: 4 })],
+      ['5x5', new Cube({ size: 5 })],
+      ['kilominx', new Dodecaminx({ size: 2 })],
+      ['megaminx', new Dodecaminx({ size: 3 })],
+      ['masterminx', new Dodecaminx({ size: 4 })],
+      ['gigaminx', new Dodecaminx({ size: 5 })],
+    ];
+
+    return {
+      puzzles,
+    };
+  },
+  components: {
+    VPuzzle,
+  },
+});
+</script>
