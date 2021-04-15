979815a9bc6482c60bdae77ab10ff412b5a13df1
Scott Bedard <scottbedard90@gmail.com>
2021-04-14 21:26:01 -0500

    add range inputs

--- client/src/components/Puzzle.vue
+++ client/src/components/Puzzle.vue
@@ -1,19 +1,40 @@
 <template>
-  <Scene square>
+  <Scene
+    :camera-angle="cameraAngle"
+    :camera-distance="cameraDistance"
+    square>
     <AxesHelper :size="1" />
   </Scene>
+
+  <div class="mt-6">
+    <div>Camera Angle: {{ cameraAngle }}</div>
+    <RangeInput v-model="cameraAngle" />
+  </div>
+
+  <div class="mt-6">
+    <div>Camera Distance: {{ cameraDistance }}</div>
+    <RangeInput v-model="cameraDistance" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
 import { AxesHelper, Scene } from '@/three/components'
+import { defineComponent, ref } from 'vue'
+import RangeInput from './RangeInput.vue'
 
 export default defineComponent({
   setup() {
-    
+    const cameraAngle = ref(0)
+    const cameraDistance = ref(0)
+
+    return {
+      cameraAngle,
+      cameraDistance,
+    }
   },
   components: {
     AxesHelper,
+    RangeInput,
     Scene,
   },
   name: 'Puzzle',
