27311ffd65432793a89fdbb8f7f26b7dd032ea11
Scott Bedard <scottbedard90@gmail.com>
2021-04-16 22:04:34 -0500

    code style

--- client/src/three/behaviors/use-geometry.ts
+++ client/src/three/behaviors/use-geometry.ts
@@ -17,36 +17,36 @@ export function useGeometry(path: MaybeRef<Vector2[]>, radius: MaybeRef<number> 
     unref(path).forEach((point, index, arr) => {
       if (normalizedRadius === 0) {
         if (index === 0) {
-          shape.moveTo(point[0], point[1]);
+          shape.moveTo(point[0], point[1])
         } else {
-          shape.lineTo(point[0], point[1]);
+          shape.lineTo(point[0], point[1])
         }
       } else {
-        const pathFromPoint = rollArray(arr, index);
+        const pathFromPoint = rollArray(arr, index)
   
         // find end point of previous curve
-        const prev = rollArray(pathFromPoint, -1).shift() as Vector2;
-        const prev2 = rollArray(pathFromPoint, -2).shift() as Vector2;
-        const prevCurveDist = Math.min(measure(point, prev) / 2, measure(prev, prev2) / 2) * normalizedRadius;
-        const prevCurveEnd = translate(prev, point, prevCurveDist);
+        const prev = rollArray(pathFromPoint, -1).shift() as Vector2
+        const prev2 = rollArray(pathFromPoint, -2).shift() as Vector2
+        const prevCurveDist = Math.min(measure(point, prev) / 2, measure(prev, prev2) / 2) * normalizedRadius
+        const prevCurveEnd = translate(prev, point, prevCurveDist)
     
         // find start / end points of current curve
-        const next = rollArray(pathFromPoint, 1).shift() as Vector2;
-        const next2 = rollArray(pathFromPoint, 2).shift() as Vector2;
-        const curveDist = Math.min(measure(point, next) / 2, measure(next, next2) / 2) * normalizedRadius;
-        const curveStart = translate(point, prev, curveDist);
-        const curveEnd = translate(point, next, curveDist);
+        const next = rollArray(pathFromPoint, 1).shift() as Vector2
+        const next2 = rollArray(pathFromPoint, 2).shift() as Vector2
+        const curveDist = Math.min(measure(point, next) / 2, measure(next, next2) / 2) * normalizedRadius
+        const curveStart = translate(point, prev, curveDist)
+        const curveEnd = translate(point, next, curveDist)
     
         // start drawing from the end of the previous curve
         if (index === 0) {
-          shape.moveTo(prevCurveEnd[0], prevCurveEnd[1]);
+          shape.moveTo(prevCurveEnd[0], prevCurveEnd[1])
         }
     
         // draw line to the start of the current curve
-        shape.lineTo(curveStart[0], curveStart[1]);
+        shape.lineTo(curveStart[0], curveStart[1])
 
         // draw the current curve
-        shape.quadraticCurveTo(point[0], point[1], curveEnd[0], curveEnd[1]);
+        shape.quadraticCurveTo(point[0], point[1], curveEnd[0], curveEnd[1])
       }
     })
 
