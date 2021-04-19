2342562d6b5658e2c3b0fb2fc6dcb80195b1d9e9
Scott Bedard <scottbedard90@gmail.com>
2021-04-18 22:38:40 -0500

    add sample example

---
+++ client/src/partials/components/_SampleExample.vue
@@ -0,0 +1,20 @@
+<template>
+  Sample component
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Button } from '@/components'
+
+export default defineComponent({
+  setup() {
+    return {
+      
+    }
+  },
+  components: {
+    Button
+  },
+  name: 'SampleExample',
+})
+</script>
