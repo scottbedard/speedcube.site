f51065dab0d1b59e39312ba98c51212cd264cc4a
Scott Bedard <scottbedard90@gmail.com>
2021-04-18 21:11:32 -0500

    scaffolding components page

---
+++ client/src/partials/components/ButtonExample.vue
@@ -0,0 +1,44 @@
+<template>
+  <Button
+    :disabled="disabled"
+    :loading="loading"
+    :primary="primary"
+    @click="onClick">
+    Click me
+  </Button>
+
+  <div class="gap-6 flex mt-6">
+    <Checkbox v-model="disabled">Disabled</Checkbox>
+    <Checkbox v-model="loading">Loading</Checkbox>
+    <Checkbox v-model="primary">Primary</Checkbox>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { Button, Checkbox } from '@/components'
+
+export default defineComponent({
+  setup() {
+    const disabled = ref(false)
+    const loading = ref(false)
+    const primary = ref(true)
+
+    const onClick = () => {
+      alert('👋 Hello!')
+    }
+
+    return {
+      disabled,
+      loading,
+      onClick,
+      primary,
+    }
+  },
+  components: {
+    Button,
+    Checkbox,
+  },
+  name: 'ButtonExample',
+})
+</script>
--- client/src/routes/Components.vue
+++ client/src/routes/Components.vue
@@ -1,6 +1,43 @@
 <template>
-  <div>
-    Hello from the components page!<br />
-    <RouterLink :to="{ name: 'home' }">Go to the home page</RouterLink>
+  <div
+    v-for="(example, index) in examples"
+    :key="index">
+    <h2 class="font-bold font-mono mb-6 text-lg">
+      <RouterLink
+        v-text="example.header"
+        :to="{
+          name: 'components',
+          query: {
+            filter: example.header,
+          }
+        }" />
+    </h2>
+    <component
+      :is="example.component" />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, defineAsyncComponent, defineComponent } from 'vue'
+
+type Example = {
+  component: Component
+  header: string
+}
+
+const examples: Example[] = [
+  {
+    component: defineAsyncComponent(() => import('@/partials/components/ButtonExample.vue')),
+    header: '<button>',
+  }
+]
+
+export default defineComponent({
+  setup() {
+    return {
+      examples,
+    }
+  },
+})
+</script>
+
