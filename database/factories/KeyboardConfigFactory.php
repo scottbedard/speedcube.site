d3efba2ae1ad26c88341a8deb377988a118deb2b
Scott Bedard <scottbedard90@gmail.com>
2021-04-28 21:36:30 -0500

    keyboard config factory

---
+++ database/factories/KeyboardConfigFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\KeyboardConfig;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class KeyboardConfigFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = KeyboardConfig::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'config' => [],
+        ];
+    }
+}
