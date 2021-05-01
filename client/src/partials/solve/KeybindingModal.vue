4658b73359b2daf12f3d33d829a69bd98146c241
Scott Bedard <scottbedard90@gmail.com>
2021-05-01 00:09:13 -0500

    scaffold keybinding form

--- client/src/partials/solve/KeybindingModal.vue
+++ client/src/partials/solve/KeybindingModal.vue
@@ -1,34 +1,54 @@
 <template>
   <Modal padded>
-    <h3>Add key binding</h3>
+    <form
+      class="gap-6 grid xs:grid-cols-2"
+      @submit.prevent="submit">
+      <Input
+        autofocus
+        class
+        label="Key"
+        placeholder="Enter character"
+        required />
 
-    <div class="flex flex-wrap gap-6 items-center justify-end">
-      <a
-        class="text-center w-full xs:w-auto"
-        href="#"
-        @click.prevent="close">
-        Cancel
-      </a>
+      <Input
+        label="Turn"
+        placeholder="Enter puzzle turn"
+        required />
 
-      <Button
-        class="w-full xs:w-auto"
-        primary>
-        Add
-      </Button>
-    </div>
+      <div class="flex flex-wrap gap-6 items-center justify-end xs:col-span-2">
+        <a
+          class="text-center w-full xs:w-auto"
+          href="#"
+          @click.prevent="close">
+          Cancel
+        </a>
+
+        <Button
+          class="w-full xs:w-auto"
+          primary
+          type="submit">
+          Add
+        </Button>
+      </div>
+    </form>
   </Modal>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Button, Modal } from '@/components'
+import { Button, Input, Modal } from '@/components'
 
 export default defineComponent({
   setup(props, { emit }) {
     const close = () => emit('close')
 
+    const submit = () => {
+      console.log('submit')
+    }
+
     return {
-      close
+      close,
+      submit,
     }
   },
   emits: [
@@ -36,6 +56,7 @@ export default defineComponent({
   ],
   components: {
     Button,
+    Input,
     Modal,
   },
 })
