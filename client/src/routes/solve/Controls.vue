74055be6af564caecb769f0d070facd97d6c176a
Scott Bedard <scottbedard90@gmail.com>
2021-05-18 22:42:06 -0500

    add empty state

--- client/src/routes/solve/Controls.vue
+++ client/src/routes/solve/Controls.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="gap-x-10 gap-y-3 flex flex-wrap justify-center mb-6">
-    <a
-      v-for="(link, index) in toolbar"
-      class="flex items-center justify-center w-full sm:w-auto"
-      href="#"
-      :key="index"
-      @click.prevent="link.onClick">
-      <IconText :name="link.icon">
-        {{ link.text }}
-      </IconText>
-    </a>
-  </div>
+  <div class="grid gap-12 max-w-6xl mx-auto">
+    <div class="gap-x-12 gap-y-3 flex flex-wrap justify-center">
+      <a
+        v-for="(link, index) in toolbar"
+        class="flex items-center justify-center w-full sm:w-auto"
+        href="#"
+        :key="index"
+        @click.prevent="link.onClick">
+        <IconText :name="link.icon">
+          {{ link.text }}
+        </IconText>
+      </a>
+    </div>
 
-  <div class="grid gap-6 max-w-6xl mx-auto">
-    <div class="flex flex-wrap font-mono gap-4 justify-center">
+    <p
+      v-if="empty"
+      class="text-center">
+      No key bindings are configured, <a href="#" @click.prevent="openAddModal">click here to get started</a>.
+    </p>
+    
+    <div
+      v-else
+      class="flex flex-wrap font-mono gap-4 justify-center">
       <button
         v-for="(turn, key) in previewKeyboardConfig"
         class="bg-gray-50 flex px-3 py-1 rounded shadow-md text-sm hover:bg-white dark:bg-gray-700 dark:hover:bg-gray-600"
@@ -97,7 +105,7 @@
 <script lang="ts">
 import { alert } from '@/app/alerts'
 import { Button, IconText } from '@/components'
-import { defineComponent, onMounted, onUnmounted, Ref, ref } from 'vue'
+import { computed, defineComponent, onMounted, onUnmounted, Ref, ref } from 'vue'
 import { isAuthenticated, keyboardConfig } from '@/app/store/computed'
 import { Keybinding } from '@/app/types/puzzle'
 import { previewKeyboardConfig } from '@/app/store/state'
@@ -125,6 +133,10 @@ export default defineComponent({
     const jsonModalIsVisible = ref(false)
     const keybindingModalIsVisible = ref(false)
     const resetModalIsVisible = ref(false)
+
+    const empty = computed(() => {
+      return !previewKeyboardConfig.value || Object.keys(previewKeyboardConfig.value).length === 0
+    })
 
     // model management
     const {
@@ -155,6 +167,13 @@ export default defineComponent({
       closeModals()
 
       isVisible.value = true
+    }
+
+    // open the add binding modal
+    const openAddModal = () => {
+      activeBinding.value = null
+
+      openModal(keybindingModalIsVisible)
     }
 
     // remove the active binding
@@ -208,10 +227,7 @@ export default defineComponent({
       {
         icon: 'plus',
         text: 'Add key binding',
-        onClick: () => {
-          activeBinding.value = null
-          openModal(keybindingModalIsVisible)
-        },
+        onClick: openAddModal,
       },
       {
         icon: 'tool',
@@ -247,10 +263,12 @@ export default defineComponent({
       clearAllModalIsVisible,
       closeModals,
       editBinding,
+      empty,
       isAuthenticated,
       jsonModalIsVisible,
       keybindingModalIsVisible,
       loading,
+      openAddModal,
       previewKeyboardConfig,
       removeActiveBinding,
       resetModalIsVisible,
